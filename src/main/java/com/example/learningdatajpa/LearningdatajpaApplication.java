e2b8e074233b19b1de1c18f5ba57bccc5705eb14
Alok-024 <AlokKumar.Maurya@cognizant.com>
2025-12-31 11:27:06 +0530

    second commit

--- src/main/java/com/example/learningdatajpa/LearningdatajpaApplication.java
+++ src/main/java/com/example/learningdatajpa/LearningdatajpaApplication.java
@@ -8,6 +8,7 @@ public class LearningdatajpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningdatajpaApplication.class, args);
+		System.out.println("Heelo dummy");
 	}
 
 }
